4313869d881f561704a986d7a159b66c60b9e544
mirik12 <mirik12mirik@gmail.com>
2023-05-29 16:35:48 +0200

    reload directories

--- Lesson-21-Terraform Module/projectA/main.tf
+++ Lesson-21-Terraform Module/projectA/main.tf
@@ -1,52 +1,44 @@
+
+#----------------------------------------------------------
+# My Terraform
+#
+# Use Our Terraform Module to create AWS VPC Networks
+#
+# Made by Mirik. 29 May 2023
+#----------------------------------------------------------
 provider "aws" {
-  region = "eu-north-1"
+  region = var.region
 }
 
-# module "vpc-default" {
-#   source = "../modules/aws_network"
-# }
+module "vpc-default" {
+  source = "../modules/aws_network"
+  //  source               = "git@github.com:mirik12/terraform-modules/aws_network"
+}
 
 module "vpc-dev" {
-#   source               = "../modules/aws_network"
-  source = "git@github.com:mirik12/terraform-modules.git/aws_network"
-  env                  = "development"
+  source = "../modules/aws_network"
+  //  source               = "git@github.com:mirik12/terraform-modules/aws_network"
+  env                  = "dev"
   vpc_cidr             = "10.100.0.0/16"
   public_subnet_cidrs  = ["10.100.1.0/24", "10.100.2.0/24"]
   private_subnet_cidrs = []
 }
 
 module "vpc-prod" {
-#   source               = "../modules/aws_network"
-  source = "git@github.com:mirik12/terraform-modules.git/aws_network"
-  env                  = "production"
+  source = "../modules/aws_network"
+  // source               = "git@github.com:mirik12/terraform-modules/aws_network"
+  env                  = "prod"
   vpc_cidr             = "10.10.0.0/16"
   public_subnet_cidrs  = ["10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24"]
   private_subnet_cidrs = ["10.10.11.0/24", "10.10.22.0/24", "10.10.33.0/24"]
 }
 
 module "vpc-test" {
-#   source               = "../modules/aws_network"
-  source = "git@github.com:mirik12/terraform-modules.git/aws_network"
-  env                  = "production"
+  source = "../modules/aws_network"
+  // source               = "git@github.com:mirik12/terraform-modules/aws_network"
+  env                  = "staging"
   vpc_cidr             = "10.10.0.0/16"
   public_subnet_cidrs  = ["10.10.1.0/24", "10.10.2.0/24"]
   private_subnet_cidrs = ["10.10.11.0/24", "10.10.22.0/24"]
 }
-
-// -------------------------------------------------
-
-output "prod_public_subnet_ids" {
-  value = module.vpc-prod.public_subnet_ids
-}
-
-output "prod_private_subnet_ids" {
-  value = module.vpc-prod.private_subnet_ids
-}
-
-output "dev_public_subnet_ids" {
-  value = module.vpc-prod.public_subnet_ids
-}
-
-output "dev_private_subnet_ids" {
-  value = module.vpc-prod.private_subnet_ids
-}
+#===============================================
